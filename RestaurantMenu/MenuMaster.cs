2591157e36719d0e60c9fb11be6334243aa1cd5a
Kirill Kalutsky <kefirchik.2000@mail.ru>
2022-10-16 08:56:55 +0300

    основная функциональность

---
+++ RestaurantMenu/MenuMaster.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantMenu
+{
+    internal class MenuMaster<T>
+    {
+        private ICollection<T> items;
+        private int pageSize;
+        public MenuMaster(ICollection<T> items, int pageSize)
+        {
+            this.items = items;
+            this.pageSize = pageSize;
+        }
+
+        public int GetItemsCount() => 
+            items.Count;
+
+        public int GetPageCount() =>
+            (int)Math.Ceiling(GetItemsCount()/(double)pageSize);
+
+        public int GetPageItemsCount(int pageNumber) => 
+            pageNumber < GetPageCount() ? pageSize : GetItemsCount() - pageSize * (pageNumber - 1);
+
+        public IEnumerable<T> GetPageItems(int pageNumber) =>
+            items.Skip((pageNumber-1)*pageSize).Take(pageSize);
+
+        public IEnumerable<T> GetFirstItemsAllPages() =>
+            items.Where((x,i)=>i%pageSize==0);
+    }
+}
---
+++ RestaurantMenu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using RestaurantMenu;
+
+var a = new string[5] {"a", "b", "c", "d", "e" };
+var menu = new MenuMaster<string>(a, 3);
+
+menu.GetFirstItemsAllPages().ToList().ForEach(Console.Write);
+Console.WriteLine();
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
